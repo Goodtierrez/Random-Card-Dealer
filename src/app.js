b21c6231c39f2fa9e4548aaad8c1773599e264a9
Jonathan Gutiérrez <jonathangutierrezperez@gmail.com>
2023-03-22 20:59:59 +0000

    poker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let suite = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let numberMain = document.querySelector(".number");
+  let suiteA = document.querySelector(".top-suite");
+  let suiteB = document.querySelector(".bottom-suite");
+
+  function randomNumber() {
+    return Math.floor(Math.random() * numbers.length);
+  }
+  let number = numbers[randomNumber()];
+
+  numberMain.innerHTML = number;
+
+  function randomSuite() {
+    return Math.floor(Math.random() * suite.length);
+  }
+
+  let suiteMain = suite[randomSuite()];
+
+  suiteA.innerHTML = suiteMain;
+  suiteB.innerHTML = suiteMain;
+
+  if (suiteMain === "♦" || suiteMain === "♥") {
+    numberMain.classList.add("red");
+    suiteA.classList.add("red");
+    suiteB.classList.add("red");
+  }
 };
